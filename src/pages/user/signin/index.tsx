a4c90da9ae9a55352cded19f60f66ca63f0ea0d9
eymin1259 <eymin1259@gmail.com>
2023-05-05 20:44:30 +0900

    feat : 로그인 페이지 ui 수정

--- src/pages/user/signin/index.tsx
+++ src/pages/user/signin/index.tsx
@@ -2,6 +2,38 @@ import Input from '@/components/common/Input';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
+export default function SignIn() {
+  const router = useRouter();
+  return (
+    <SignInWrapper>
+      <div className="signin-box">
+        <div className="signin-section">
+          <h1>Webling Cafe</h1>
+        </div>
+
+        <form>
+          <div className="signin-section">
+            <Input className="signin-input" type="email" placeholder="email"></Input>
+            <Input className="signin-input" type="password" placeholder="password"></Input>
+          </div>
+
+          <div className="signin-section">
+            <button>로그인</button>
+            <button
+              type="button"
+              onClick={() => {
+                router.push('/user/signup');
+              }}
+            >
+              회원가입
+            </button>
+          </div>
+        </form>
+      </div>
+    </SignInWrapper>
+  );
+}
+
 const SignInWrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -22,7 +54,8 @@ const SignInWrapper = styled.div`
     justify-content: center;
     align-items: center;
     margin-bottom: 20px;
-    > * {
+
+    .signin-input {
       margin-bottom: 10px;
     }
   }
@@ -35,32 +68,6 @@ const SignInWrapper = styled.div`
     border-radius: 10px;
     background-color: #959595;
     color: white;
+    margin-bottom: 10px;
   }
 `;
-
-export default function SignIn() {
-  const router = useRouter();
-  return (
-    <SignInWrapper>
-      <div className="signin-box">
-        <div className="signin-section">
-          <h1>Webling Cafe</h1>
-        </div>
-        <div className="signin-section">
-          <Input></Input>
-          <Input></Input>
-        </div>
-        <div className="signin-section">
-          <button>로그인</button>
-          <button
-            onClick={() => {
-              router.push('/user/signup');
-            }}
-          >
-            회원가입
-          </button>
-        </div>
-      </div>
-    </SignInWrapper>
-  );
-}
